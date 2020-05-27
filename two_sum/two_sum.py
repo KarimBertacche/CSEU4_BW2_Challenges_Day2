d39da2a680badf465f88cb5194d1d45408f096db
Karim B <mattiakarim@live.it>
2020-05-27 10:51:34 +0200

    Complete two_sum challenge

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for 
+        for idx in range(len(nums)):
+            if nums[idx] > target:
+                continue
+
+            for idx2 in range(idx + 1, len(nums)):
+                if nums[idx] + nums[idx2] == target:
+                    return [ idx, idx2 ]
+
+            return None
+
+obj = Solution()
+
+print(obj.twoSum([2, 7, 11, 15], 9))
